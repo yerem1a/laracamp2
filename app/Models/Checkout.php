76edb911988b75b444bc2b1d4e36da41a7d58d5f
yeremia purba <yeremiapurba14@gmail.com>
2023-08-13 15:47:19 +0700

    integrasi co dan slicing dashboard

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -9,5 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired'.'cvc', 'is_paid'];
+    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired','cvc', 'is_paid'];
+
+    public function setExpiredAttribute($value){
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,20 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout/{camp:slug}', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
-
-// socialite route 
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+    
+    // socialite route 
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
