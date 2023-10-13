1598c6ca3a80e6708a561499932997baed4f6c1b
yeremia purba <yeremiapurba14@gmail.com>
2023-10-13 16:55:47 +0700

    admin dashboard pt1

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\User\DashboardController as UserDashboard;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,6 +21,16 @@ Route::middleware(['auth'])->group(function(){
     
     // socialite route 
     Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+
+    // user dashboard 
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->group(function(){
+        Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
+    });
+
+    // admin dashboard 
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->group(function(){
+        Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+    });
 });
 
 require __DIR__.'/auth.php';
